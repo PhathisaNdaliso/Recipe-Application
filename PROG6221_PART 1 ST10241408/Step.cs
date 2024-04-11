9523280297be8d25f44b1f6fd467bc18cbc5dd38
Phathisa Ndaliso <130817265+ST10241408@users.noreply.github.com>
2024-04-11 11:29:41 +0000

    Add files via upload

---
+++ PROG6221_PART 1 ST10241408/Step.cs
@@ -0,0 +1,13 @@
+﻿// Step class to represent steps in the recipe.
+public class Step
+{
+    //description of step 
+    public string Description { get; set; }
+
+    //constructor to initialize the step obj with description
+    public Step(string description)
+    {
+        Description = description;
+    }
+}
+
